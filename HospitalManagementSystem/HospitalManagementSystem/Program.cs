15054d4b6c43cfb18008396843cd0dd00be3752d
Nam <nam.dang@daemon.com.au>
2023-09-01 01:21:40 +1000

    Initialise project

---
+++ HospitalManagementSystem/HospitalManagementSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace HospitalManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
